a32933c565f8b345473ecf5c64b0e21b08cb3125
Nsanawaji Adasi <nsanawajiadasi@gmail.com>
2023-05-26 23:39:20 +0000

    Editing

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+    if number > 10:
+        print(f"0{number}, ")
     else:
-        print("{:02}".format(number), end=", ")
+        print(f"{number}, ")
